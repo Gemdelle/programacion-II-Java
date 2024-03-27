9c0edc784503dd46cd675a5615364fcd0afa43fc
Gemdelle <milu200911@hotmail.com>
2024-03-27 17:29:26 -0300

    update.

--- java_projects/practice_0/exercise_04/src/Main.java
+++ java_projects/practice_0/exercise_04/src/Main.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Vendedor vendedor1 = new Vendedor(1);
-        List<Nota>[] notasVendedor1 = vendedor1.generarNotasPorVendedor();
+        List<Nota>[] notasVendedor1 = vendedor1.generarNotasPorVendedorPorMes();
         vendedor1.impresion(notasVendedor1);
     }
 }
--- java_projects/practice_0/exercise_04/src/Vendedor.java
+++ java_projects/practice_0/exercise_04/src/Vendedor.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +14,12 @@ public class Vendedor {
     }
 
     // 03. METHODS
-    public List<Nota>[] generarNotasPorVendedor() {
-        int dias = 29;
-        List<Nota>[] diasMes = new ArrayList[dias]; // crear array de listas con 30 lugares
+    public List<Nota>[] generarNotasPorVendedorPorMes() {
+        int dias = 30;
+        List<Nota>[] diasMes = new ArrayList[dias]; // crear array de listas con 30 lugares, del 0-29
 
 
-        Random random = new Random(); // nextInt(5) -> va a devolver 5 posiciones, pero le suma 1 al resultado para que el 0 sea 1.
+        Random random = new Random(); //
         int cantidadNotas = 0;
         // CREAR VARIABLE PARA QUE EL PRODUCTO SEA RANDOM ENTRE 1 Y 5
 
@@ -26,12 +27,23 @@ public class Vendedor {
 
         for (int i = 0; i < dias; i++)
         { // para cada día del mes
-            cantidadNotas = random.nextInt(5) + 1; // cambiar la cantidad de notas por día
-            List<Nota> notasPorDia = new ArrayList<>();
+
+            // VARIABLES PARA CREAR LAS NOTAS DE VENDEDOR POR DÍA
+            cantidadNotas = random.nextInt(5) + 1; // cambiar la cantidad de notas por día. nextInt(5) -> va a devolver 5 posiciones, pero le suma 1 al resultado para que el 0 sea 1.
+            List<Integer> numeroProductos =  new ArrayList<>(); // Lista que va a guardar los números 1-5, que son los números de producto posibles.
+            numeroProductos.addAll(Arrays.asList(1, 2, 3, 4, 5)); // Agregar los números 1-5 a la lista.
+            for(int c = 0;c < cantidadNotas;c++) {
+                int randomIndex = random.nextInt(numeroProductos.size()); // Obtener un índice aleatorio dentro del rango de la lista.
+                numeroProductos.remove(randomIndex); // Eliminar el elemento en el índice aleatorio.
+            } // Al terminar el for, va a quedar una lista con productos random que se corresponde con la CANTIDAD de notas generadas también de manera random para ese día.
+            List<Nota> notasPorDia = new ArrayList<>(); // Lista que va a guardar las notas de cada día
+
             for (int j = 0; j < cantidadNotas; j++)
             {
-                Nota nota = new Nota(numeroVendedor, j, ganancia);
+                int numeroDeProducto = numeroProductos.get(random.nextInt(numeroProductos.size()));
+                Nota nota = new Nota(numeroVendedor, numeroDeProducto, ganancia);
                 notasPorDia.add(nota); // una lista con las notas que hay por día
+                numeroProductos.remove(numeroDeProducto);
             }
             diasMes[i] = notasPorDia; // array de arrays
         }
