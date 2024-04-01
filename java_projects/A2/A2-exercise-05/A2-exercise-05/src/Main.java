890bfc7529bbec321f62a82de82d3bee94f0a9a3
Gemdelle <milu200911@hotmail.com>
2024-04-01 12:38:56 -0300

    project creation.

---
+++ java_projects/A2/A2-exercise-05/A2-exercise-05/src/Main.java
@@ -0,0 +1,7 @@
+// A2 EXERCISE 05
+// Modifique la aplicación que administra DVD y CD utilizando el concepto de herencia.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
