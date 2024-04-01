51c84cdb4e2e32c1d084e558bdf012cbf60de8a3
Gemdelle <milu200911@hotmail.com>
2024-04-01 11:24:37 -0300

    project creation.

---
+++ java_projects/A2/A2-exercise-02/A2-exercise-03/src/Main.java
@@ -0,0 +1,14 @@
+// A3-EXERCISE 03
+//Supongamos que el departamento de sistemas de la empresa “Mi Luz S.A.” debe desarrollar un software para la liquidación de los haberes de sus empleados. La empresa cuenta con cuatro tipos de empleados:
+//	empleados asalariados
+//	empleados por horas
+//	empleados por comisión
+//	empleados base más comisión
+//
+//Todo empleado tiene legajo y nombre y apellido. Un empleado asalariado tiene un sueldo mensual. Un empleado por horas tiene un sueldo básico, el número de horas trabajadas y el valor horario. Un empleado por comisión tiene una tasa de comisión y la cantidad e ventas en pesos. Un empleado por comisión tiene un sueldo base, una tasa y la cantidad de ventas.
+//Cada clase debe tener constructores apropiados, los métodos get y set. Escriba un programa que cree instancias de objetos de cada una de estas clases y que muestre toda la información asociada con cada objeto (incluyendo la información heredada).
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
