ce2e1f0f17cd8ab4265ebb30f39eb99ef1774433
gemy <mili200911@hotmail.com>
2024-04-19 11:23:00 -0300

    Base.

---
+++ java_projects/A3/A3-exercise-08/exercise-08/src/Main.java
@@ -0,0 +1,35 @@
+// A3 EXERCISE 08
+// La empresa “Never-NO” realiza transportes de diferentes tipos de carga, utilizando carreteras nacionales y provinciales. Para ello requiere vehículos especialmente acondicionados según la carga.
+//Cada transporte se identifica por el número de patente y se mantiene la siguiente información:
+//descripción de la carga
+//localidad de salida
+//localidad de llegada
+//Las localidades están codificadas según el registro de códigos postales vigente.
+
+//Datos adicionales según la carga a transportar:
+// Alimentos: Lista de tipos de alimentos, cantidad a transportar de cada uno y su peso.
+// Bultos: Cantidad de bultos de cada tamaño
+// Bulto MINI: 0.01 a 20 kg.
+// Bulto MID: 21 a 100 kg.
+// Bulto MAX: 101 a 150 kg.
+//Para el cálculo de peso se considera un promedio de dicho rango.
+
+// Ganado: Cantidad de cabezas a transportar y peso promedio por cabeza
+
+// Escribir un programa aplicando el concepto de polimorfismo que muestre el costo del viaje para un vehículo dado, sabiendo que éste depende no sólo del trayecto a realizar sino de la carga a transportar.
+// Costo por peso transportado:
+// Hasta 1200kg.: $ 6.00 por kg.
+// Hasta 2400kg.: $ 5.50 por kg.
+// Hasta 4000kg.: $ 4.30 por kg.
+// Más de 4000kg.: $ 3.60 por kg.
+
+// Los transportes se realizan entre 10 localidades. No siempre la tarifa de viajar de una localidad a otra es igual en caso de realizar el viaje inverso. Existe una matriz disponible de tarifas de transporte entre ciudades.
+
+// Escribir un método (y mostrar su uso) que, para dos transportes A y B, indique aquél de mayor costo.
+// Escribir un método en la clase aplicación que reciba un conjunto de transportes y muestre en pantalla su información según el tipo de transporte.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
